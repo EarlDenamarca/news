bc9a30fee5071afbba34f817c88a9f524f7cb03f
Earl Denamarca <denamarca.earl@gmail.com>
2025-10-28 14:47:59 +0800

    add custom validation for the command parameters

--- app/Console/Commands/FetchNews.php
+++ app/Console/Commands/FetchNews.php
@@ -3,34 +3,93 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use jcobhams\NewsApi\NewsApi;
+use App\Services\SourceService;
+use App\Services\NewsApiService;
+use App\Services\ArticleService;
+use Carbon\Carbon;
 
 class FetchNews extends Command
 {
+    private SourceService   $source_service;
+    private NewsApiService  $news_api_service;
+    private ArticleService  $article_service;
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'news:fetch ';
+    protected $signature = 'news:fetch
+        {--category= : category of the news}
+        {--from= : format(YYYY-MM-DD)}
+        {--to= : format(YYYY-MM-DD)}
+        {--limit= : number of articles to be retreived}
+        {--page= : page number}';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'fetch news articles from external sources';
 
     /**
      * Execute the console command.
      */
-    public function handle()
+    public function handle( 
+        SourceService   $source_service,
+        NewsApiService  $news_api_service,
+        ArticleService  $article_service
+    )
     {
-        $news_api_key   = env('NEWS_API');
-        $newsapi        = new NewsApi($news_api_key);
+        $category   = $this->option( 'category' );
+        $from       = $this->option( 'from' );
+        $to         = $this->option( 'to' );
+        $limit      = $this->option( 'limit' );
+        $page       = $this->option( 'page' );
         
-        // $sorts          = $newsapi->getSortBy();
-        $news = $newsapi->getEverything('', 'nhl-news', '', '', '2025-10-01', '2025-10-28', 'en', 'publishedAt',  3, 1);
-        dd( $news );
+        if ( !$category ) {
+            return $this->error( '--category parameter is required (eg. business, entertainment, general, health, science, sports, technology)' );
+        }
+
+        if ( !$from ) {
+            return $this->error( '--from parameter is required. Sample format (YYYY-MM-DD)' );
+        }
+
+        if ( !$to ) {
+            return $this->error( '--to parameter is required. Sample format (YYYY-MM-DD)' );
+        }
+
+        if ( !$limit ) {
+            return $this->error( '--limit parameter is required' );
+        }
+
+        if ( !$page ) {
+            return $this->error( '--page parameter is required' );
+        }
+
+        $this->source_service   = $source_service;
+        $this->news_api_service = $news_api_service;
+        $this->article_service  = $article_service;
+
+        $from       = Carbon::createFromFormat( 'Y-m-d', $from )->format( 'Y-m-d' );
+        $to         = Carbon::createFromFormat( 'Y-m-d', $to )->format( 'Y-m-d' );
+        $sources    = $this->source_service->getAllSources();
+        $sources    = $sources->pluck( 'source_id' )->join( ',' );
+
+        $news       = $this->news_api_service->fetchArticles( $sources, $from, $to, $limit, $page );
+
+        foreach ( $news as $article ) {
+            $this->article_service->storeArticle(
+                $title,
+                $description,
+                $content,
+                $author,
+                $source_id,
+                $published_date,
+                $url,
+                $image_url
+            );
+        }
     }
 }
